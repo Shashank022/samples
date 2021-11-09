55509a006cb2e20d0faddceccbb904905bde5d4f
Shashank022 <shashank022@gmail.com>
2021-11-09 15:48:42 -0600

    updated changes

--- addednew.js
+++ addednew.js
@@ -1,4 +1,3 @@
-
 let num1 = "123";
 let num2 = "6";
 let store=[];
@@ -9,7 +8,7 @@ for (let i = num1.length - 1; i >= 0; i--) {
         let result1 = result.toString();
         if (result1.length >= 1) {
             let temp = result1.charAt(1);
-            temp = Number(temp) + Number(result1.charAt(1));
+            temp = Number(temp) + Number(result1.charAt(0));
             store.push(temp);
         }
     }
