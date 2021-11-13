9bf69edfbd25d7ec135a09975c21818ce0df071a
Shashank <shashank022@gmail.com>
2021-11-12 20:57:49 -0600

    Added few changes

--- tripledigit.js
+++ tripledigit.js
@@ -1,36 +1,37 @@
-let num1 = "868";
-let num2 = "564";
+let num1 = "367";
+let num2 = "251";
 let store=[];
 let resultStore=[];
 let oddOnes = [];
 let evenOnes = [];
 
-for(let i = 0; i <=num1.length; i++){
-    let result = Number(num1.charAt(i)) * Number(num2.charAt(2));
-    store.push(result);
+for(let i = 0; i < num1.length; i++){
+    let first = Number(num1.charAt(i)) * Number(num2.charAt(2));
+    if(first.toString().length <= 1){
+        first = "0"+first;
+    }
+    store.push(first);
+    let second = Number(num1.charAt(i)) * Number(num2.charAt(1));
+    if(second.toString().length <= 1){
+        second = "0"+second;
+    }
+    oddOnes.push(second);
+    let third = Number(num1.charAt(i)) * Number(num2.charAt(0));
+    if(third.toString().length <= 1){
+        third = "0"+third;
+    }
+    evenOnes.push(third);
 }
 
-console.log(store);
+console.log("store",store);
+console.log("oddOnes",oddOnes);
+console.log("evenOnes",evenOnes);
 
-// for (let i = num1.length - 1; i >= 0; i--) {
-//     for (let j = num2.length - 1; j >= 0; j--) {
-//         let result = (num1.charAt(i) * num2.charAt(j)).toString();
-//         if(result.length <= 1){
-//             result = "0"+result;
-//         }
-//         store.push(result);
-//     }
-// }
-// for (let k = 0; k < store.length; k += 2) {
-//     oddOnes.push(store[k]);
-//     store[k+1] && evenOnes.push(store[k + 1]);
-// }
-//
-// let firstdigit = oddOnes[0].charAt(1);
-// let scnddigit = Number(oddOnes[0].charAt(0)) + Number(oddOnes[1].charAt(1)) + Number(evenOnes[0].charAt(1));
-// let thirddigit = Number(oddOnes[1].charAt(0)) + Number(oddOnes[2].charAt(1)) + Number(evenOnes[0].charAt(0))+ Number(evenOnes[1].charAt(1));
-// let fourthdigit = Number(oddOnes[2].charAt(0)) + Number(evenOnes[1].charAt(0)) + Number(evenOnes[2].charAt(1));
-// let fifthdigit = Number(evenOnes[2].charAt(0));
+let firstdigit = store[2].charAt(1);
+let scnddigit = Number((oddOnes[0].toString()).charAt(0)) + Number((oddOnes[1].toString()).charAt(1)) + Number((evenOnes[0].toString().toString()).charAt(1));
+let thirddigit = Number(oddOnes[1].toString().charAt(0)) + Number(oddOnes[2].toString().charAt(1)) + Number(evenOnes[0].toString().charAt(0))+ Number(evenOnes[1].toString().charAt(1));
+let fourthdigit = Number(oddOnes[2].toString().charAt(0)) + Number(evenOnes[1].toString().charAt(0)) + Number(evenOnes[2].toString().charAt(1));
+let fifthdigit = Number(evenOnes[2].toString().charAt(0));
 //
 // if(scnddigit.toString().length >=2){
 //     thirddigit = thirddigit + Number(scnddigit.toString().substring(0,1));
@@ -47,5 +48,11 @@ console.log(store);
 //     fourthdigit = fourthdigit.toString().substring(1,2);
 // }
 //
+
+console.log("firstdigit",firstdigit);
+console.log("scnddigit", scnddigit)
+console.log("thirddigit",thirddigit);
+console.log("fourthdigit", fourthdigit)
+console.log("fifthdigit", fifthdigit)
 // resultStore = [fifthdigit,fourthdigit,thirddigit,scnddigit,firstdigit]
 // console.log((resultStore.join("")));
