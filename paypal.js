017bf359b3f8b696cd44df5616d0aed0b480fde6
Shashank <shashank022@gmail.com>
2021-11-13 21:15:08 -0600

    Added few changes

---
+++ paypal.js
@@ -0,0 +1,14 @@
+
+let sInput = "PAYPALISHIRING";
+let numRows = 3;
+
+//console.log(reverse(sInput));
+arrange(sInput,numRows);
+
+function reverse(s){
+    return s.split("").reverse().join("");
+}
+
+function arrange(sInput, rowNum){
+
+}
---
+++ paypalhiring.js
@@ -0,0 +1,10 @@
+
+let sInput = "PAYPALISHIRING";
+let numRows = 3;
+
+console.log(reverse(sInput));
+
+function reverse(s){
+    return s.split("").reverse().join("");
+}
+
